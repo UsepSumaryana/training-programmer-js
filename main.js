05b5a2de361b37df9709adc06c67d54b486e7ef9
neuron.usep <usep.neuronworks.co.id>
2023-11-17 00:54:39 +0700

    Asynchronous function untuk fetch data API

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-const products = [
-    { id: 1, name: "Mouse", price: 250000 },
-    { id: 2, name: "Keyboard", price: 300000 },
-    { id: 3, name: "Monitor", price: 1000000 },
-]
+// Asynchronous function untuk fetch data dari API
+const getProducts = async () => {
+    // Fetch data dari API
+    fetch("https://6554347063cafc694fe63a4b.mockapi.io/api/v1/products")
+        .then((response) => response.json())
+        .then((data) => {
+            // simpan di local storage
+            localStorage.setItem('products', JSON.stringify(data));
+        });
+};
+// call
+getProducts();
 
-let cart = JSON.parse(localStorage.getItem('cart')) ?? [];
+let products = JSON.parse(localStorage.getItem("products")) ?? [];
+let cart = JSON.parse(localStorage.getItem("cart")) ?? [];
 
 // hitung jumlah item yang ada di keranjang
 let totalCartItem = cart.length;
 
 // hitung total harga item yang ada di keranjang
-let totalPrice = cart.reduce((total, item) => total + item.price, 0);
+let totalPrice = cart.reduce((total, item) => total + parseFloat(item.price), 0);
 
 // Fungsi untuk menambahkan item ke keranjang
 function addToCart(productId) {
@@ -30,33 +38,34 @@ function addToCart(productId) {
     }
 
     // update local storage
-    localStorage.setItem('cart', JSON.stringify(cart));
+    localStorage.setItem("cart", JSON.stringify(cart));
 
     // refresh data total
-    totalPrice = cart.reduce((total, item) => total + item.price, 0);
+    totalPrice = cart.reduce((total, item) => total + parseFloat(item.price), 0);
     totalCartItem = cart.length;
 }
 
 // penggunaan variabel global untuk menghitung total + ongkos kirim
-const shippingCost = 13000;
+const shippingCost = 50;
 let totalPriceShipping = totalPrice + shippingCost;
 
 // fungsi untuk menghitung total harga setelah diskon dengan scope variabel
 let getDiscountedTotalPrice = (discount) => {
-    const couponDiscount = 200000;
+    const couponDiscount = 20;
     return totalPriceShipping - discount - couponDiscount;
 };
 
 // coba diskon
-let discountedTotalPrice = getDiscountedTotalPrice(200000);
+let discountedTotalPrice = getDiscountedTotalPrice(20);
 
 // checkout handler
 const checkoutHandler = () => {
-    alert(`Total yang harus di bayar : ${discountedTotalPrice}\n\nTerimakasih sudah checkout :)`)
-}
+    alert(
+        `Total yang harus di bayar : ${discountedTotalPrice}\n\nTerimakasih sudah checkout :)`
+    );
+};
 
 console.log(`Jumlah Item di Keranjang : ${totalCartItem}`);
 console.log(`Total harga item di keranjang : ${totalPrice}`);
 console.log(`Total harga dengan ongkos kirim : ${totalPriceShipping}`);
 console.log(`Total harga setelah diskon : ${discountedTotalPrice}`);
-

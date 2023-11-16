11b2dfcc5668fcfa65ad6811ac951759b931d1d0
neuron.usep <usep.neuronworks.co.id>
2023-11-16 22:30:57 +0700

    membuat perhitungan total harga

--- main.js
+++ main.js
@@ -7,5 +7,19 @@ const cartItems = [
 let username = "usep";
 let totalCartItem = cartItems.length;
 
+let totalPrice = 0;
+
+// hitung total harga yang ada di keranjang
+for (const item of cartItems) {
+    totalPrice += item.price;
+}
+
+// pengen di format ke rupiah ah
+let totalInRupiah = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+}).format(totalPrice);
+
 console.log(`Username : ${username}`);
 console.log(`Jumlah Item di Keranjang : ${totalCartItem}`);
+console.log(`Total harga item di keranjang : ${totalInRupiah}`);

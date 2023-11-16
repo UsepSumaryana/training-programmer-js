2828cfb627619862b2ed3811ea3a7aa4ebba0732
neuron.usep <usep.neuronworks.co.id>
2023-11-16 22:15:49 +0700

    update totalCartItem

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-let username = 'usep';
-let totalCartItem = 0;
+const cartItems = [
+    { name: "Mouse", price: 250000 },
+    { name: "Keyboard", price: 300000 },
+    { name: "Monitor", price: 1000000 },
+];
+
+let username = "usep";
+let totalCartItem = cartItems.length;
 
 console.log(`Username : ${username}`);
 console.log(`Jumlah Item di Keranjang : ${totalCartItem}`);
